dc75f64ee38d36a6ce3205280bd2d59d2e88d2e0
Vinicius Ghizi <vinicius.ghizi@cwi.com.br>
2024-06-24 00:04:56 -0300

    supplier N/I

---
+++ scripts/orders/supplier.py
@@ -0,0 +1,14 @@
+from pyshadow.main import Shadow
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+import logging
+
+from variables.constants import PHONE,PHONE_MAGIE_BANK
+from scripts.login import login
+
+
+def create_order_supplier(driver: WebDriver, nuvemshop_order):
+    return print("not implemented yet")
+
