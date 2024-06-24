76ffc0c6bfce35264dd026030b1b38b283715600
Vinicius Ghizi <vinicius.ghizi@cwi.com.br>
2024-06-24 16:55:15 -0300

    notifications fix

--- scripts/utils/notifications.py
+++ scripts/utils/notifications.py
@@ -11,24 +11,18 @@ from variables.constants import PHONE,X_SEND_MESSAGE_BUTTON
 
 def whatsapp_notification(driver: WebDriver,message):
     driver.get("https://web.whatsapp.com")
-    time.sleep(5)
+    time.sleep(10)
     
-    receiver = PHONE + urllib.parse.quote_plus(str(message)) 
+    receiver = f"https://web.whatsapp.com/send/?phone={PHONE}&text={urllib.parse.quote_plus(str(message))}" 
     driver.get(receiver)
     while len (driver.find_elements(By.ID, 'side')) < 1:
         time.sleep(1)
         logging.warn("Waiting Whatsapp")
     
-    time.sleep(15)
+    time.sleep(10)
     
     button = driver.find_element(By.XPATH, X_SEND_MESSAGE_BUTTON )
-
     button.click()
-    
-    
 
     logging.warn(f"Sended message to Whatsapp: {message} ")
-    
-
-
-    
+    time.sleep(85)
