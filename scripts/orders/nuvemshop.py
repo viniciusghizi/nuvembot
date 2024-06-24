d67468481e651a8954fe982c109fbb8cecb3d146
Vinicius Ghizi <vinicius.ghizi@cwi.com.br>
2024-06-23 22:16:40 -0300

    get orders

---
+++ scripts/orders/nuvemshop.py
@@ -0,0 +1,65 @@
+from pyshadow.main import Shadow
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+from classes.order_class import order_class as Order
+import logging, time
+
+from variables.constants import URL_NUVEMSHOP,ID_USER_NUVEM, ID_PASS_NUVEM, USER_NUVEMSHOP,PASS_NUVEMSHOP,URL_CONFIRMED_PAYMENT, X_ORDER_NUMBER, X_ORDER_DETAIL_NAME, X_ORDER_DETAIL_VARIATION, X_ORDER_DETAIL_QUANTITY, X_EMBALADO_BUTTON, X_PRINT_BUTTON, X_PRODUCTS_QUANTITY, X_PDF_PRINT, MESSAGE_ORDER_CHECKED, MESSAGE_ORDER_NOT_FOUNDED
+from scripts.login import login
+from scripts.utils.notifications import whatsapp_notification
+
+
+def consult_created_orders(driver: WebDriver) :
+    wait = WebDriverWait(driver, 200)
+    
+    driver.get(URL_NUVEMSHOP)
+    logging.warning("1 - Logging NUVEMSHOP")
+    
+    login( ID_USER_NUVEM , ID_PASS_NUVEM , USER_NUVEMSHOP , PASS_NUVEMSHOP ,  driver )
+    orders = get_order( driver , wait )
+
+    if len(orders) > 0:
+        whatsapp_notification( driver , MESSAGE_ORDER_CHECKED )
+        whatsapp_notification( driver , orders )
+    #print_order(driver)
+    else :
+        whatsapp_notification( driver, MESSAGE_ORDER_NOT_FOUNDED )
+
+def print_order(driver: WebDriver) :
+    driver.find_element(By.XPATH, X_PRINT_BUTTON).click()
+    time.sleep(10)
+
+    logging.warning("3 - Printing Order")
+    driver.find_element(By.XPATH, X_PDF_PRINT).click()
+
+
+def update_order(driver: WebDriver) :
+    return driver.find_element(By.XPATH, X_EMBALADO_BUTTON).click()
+
+def get_order(driver: WebDriver, wait: WebDriverWait):
+    driver.get(URL_CONFIRMED_PAYMENT)
+    logging.warning("2 - Getting Order Details")
+    
+    time.sleep(5)
+
+    order_number = driver.find_element( By.XPATH, X_ORDER_NUMBER ) 
+    order_number.click()
+    
+    time.sleep(5)
+
+    max_quantity = driver.find_element(By.XPATH, X_PRODUCTS_QUANTITY).text.split(" ")[0]
+    orders = []
+    
+    for i in range(int(max_quantity)):
+        item_name = driver.find_element( By.XPATH,X_ORDER_DETAIL_NAME.replace( "#" , str(i+1) ) ).text
+        item_quantity = driver.find_element( By.XPATH,X_ORDER_DETAIL_QUANTITY.replace("#", str(i+1) ) ).text.split("x")[0]
+        item_variation = driver.find_element( By.XPATH,X_ORDER_DETAIL_VARIATION.replace("#", str(i+1) ) ).text.split(",")
+        
+        orders.append(Order( item_name, item_quantity , item_variation ))
+
+        logging.warn(f"Added item: {orders[i]}")
+    logging.warn(f"Order: {orders}")
+    return orders
