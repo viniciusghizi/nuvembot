c6d388b62a0046607d39bab3c7144fb4c75d6644
Vinicius Ghizi <vinicius.ghizi@cwi.com.br>
2024-06-25 16:27:47 -0300

    supplier feature initialized

---
+++ classes/supplier_class.py
@@ -0,0 +1,9 @@
+from dataclasses import dataclass
+
+@dataclass
+class supplier_class:
+    access_key: str
+    mail: int
+    supplier_order: list
+    shipping_label: str
+    content_label:str
--- scripts/orders/supplier.py
+++ scripts/orders/supplier.py
@@ -5,10 +5,16 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 import logging
 
-from variables.constants import PHONE,PHONE_MAGIE_BANK
+from classes.supplier_class import supplier_class as Supplier
+from classes.order_class import order_class as Order
 from scripts.login import login
-
+from messages import messages as Messages
 
 def create_order_supplier(driver: WebDriver, nuvemshop_order):
     return print("not implemented yet")
 
+def upload_order(driver: WebDriver, supplier: Supplier):
+    return print("not implemented yet")
+
+def buy_itens(driver: WebDriver, order: Order):
+    return print("not implemented yet")
